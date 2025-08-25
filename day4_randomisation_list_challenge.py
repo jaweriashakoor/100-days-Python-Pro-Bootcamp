2bf52eaec5dd623431f9d3118a955ba245b1467e
jaweriashakoor <jaweriashakoor37@gmail.com>
2025-08-25 01:26:10 -0700

    Delete day4_randomisation_list_challenge.py

--- day4_randomisation_list_challenge.py
+++
@@ -1,7 +0,0 @@
-import random
-friends=["Jaweria","Fiza","Esha","Atiya"]
-print(random.choice(friends))
-
-# 2nd option
-random_index=random.randint(0,3)# range of total indexes
-print(friends[random_index])
